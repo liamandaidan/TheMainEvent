dd0e7b4434c9eadbb36e35c38202e2c496c596be
Haywo <ru8391@gmail.com>
2022-07-24 11:24:42 -0600

    update specific D

--- backend/CalenderEvents.js
+++ backend/CalenderEvents.js
@@ -61,89 +61,33 @@ function retrieve() {
   xhr.send();
 }
 
-// function search() {
-//   let output =
-//     "<h2>Results</h2><table>" +
-//     "<tr><th>" +
-//     "title" +
-//     "</th><th>" +
-//     "event_group" +
-//     "</th>" +
-//     "<th>" +
-//     "notes" +
-//     "</th><th>" +
-//     "all_dates" +
-//     "</th><th>Maps Link</th></tr>";
-//   for (let i = 0; i < db.length; i++) {
-//     if (query === event_type[i]) {
-//       url = "https://www.google.ca/maps/place/" + long[i] + " " + lat[i];
-//       output +=
-//         "<tr><td>" +
-//         title[i] +
-//         "</td>" +
-//         "<td>" +
-//         event_type[i] +
-//         "</td>" +
-//         "<td>" +
-//         notes[i] +
-//         "</td>" +
-//         "<td>" +
-//         all_dates[i] +
-//         "</td>" +
-//         '<td><a target="_blank" href="' +
-//         url +
-//         '">Click Me!</a></td></tr>';
-//     } else {
-//       url = "https://www.google.ca/maps/place/" + long[i] + " " + lat[i];
-//       output +=
-//         "<tr><td>" +
-//         title[i] +
-//         "</td>" +
-//         "<td>" +
-//         event_type[i] +
-//         "</td>" +
-//         "<td>" +
-//         notes[i] +
-//         "</td>" +
-//         "<td>" +
-//         all_dates[i] +
-//         "</td>" +
-//         '<td><a target="_blank" href="' +
-//         url +
-//         '">Click Me!</a></td></tr>';
-//     }
-//   }
-//   document.getElementById("display").innerHTML = output;
-// }
+
 
 function show(db) {
   var output = "<h2>July 25</h2><table>" +
     "<tr><th>" +
-    "title" +
+    "Title" +
     "</th><th>" +
-    "event_group" +
+    "Event_group" +
     "</th>" +
     "<th>" +
-    "notes" +
+    "Notes" +
     "</th><th>" +
-    "all_dates" +
+    "All_dates" +
     "</th><th>Maps Link</th></tr>";
   // var arry = JSON.parse(xhr.responseText);
-  var pickDay = "July 25";
+  var pickDay = "Jul 25";
   for (var i = 0; i < db.length; i++) {
-    console.log(all_dates[i])
     if (all_dates[i].includes(pickDay)) {
-      console.log("HELLO");
-      output += "<tr><td>" + obj.title[i];
-      output += "&emsp;</td><td>" + obj.event_group[i];
-      output += "&emsp;</td><td>" + obj.notes[i];
-      output += "&emsp;</td><td>" + obj.all_dates[i];
-      output += "&emsp;</td><td><a href=\"https://www.google.com/maps/search/?api=1&query=" + obj.latitude_degrees;
-      output += "," + obj.longitude_degrees[i];
-      output += "\" target=\"_blank\">Click here to see map</a></td></tr></table>";
+      output += "<tr><td>" + title[i];
+      output += "&emsp;</td><td>" + event_group[i];
+      output += "&emsp;</td><td>" + notes[i];
+      output += "&emsp;</td><td>" + all_dates[i];
+      output += "&emsp;</td><td><a href=\"https://www.google.com/maps/search/?api=1&query=" + lat[i];
+      output += "," + long[i];
+      output += "\" target=\"_blank\">Click here</a></td></tr></table>";
     }
   }
-  console.log(output)
   document.getElementById("display").innerHTML = output;
-
+  console.log(output)
 }
