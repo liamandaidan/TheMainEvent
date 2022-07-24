d0ad3c1435778efa558f2c8803dd309ab15a6705
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-07-23 19:52:07 -0600

    Fin

---
+++ backend/CalenderEvents.js
@@ -0,0 +1,69 @@
+var xhr = new XMLHttpRequest(); //handles external url search
+var data = new XMLHttpRequest(); //handles our data html pages
+var db;
+//ALL PARAMETERS OF DB===========================================
+var address;
+var notes;
+var title;
+var event_type;
+var event_group;
+var more_info_url;
+var all_dates;
+var next_date;
+var lat;
+var long;
+//===============================================================
+
+/**
+ * Retrieve Data from Calgary API
+ * @param {*} field1
+ * @param {*} field2
+ * @param {*} field3
+ * @param {*} field4
+ */
+function retrieve() {
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      db = JSON.parse(xhr.responseText);
+      console.log("Database Ready");
+      address = [];
+      notes = [];
+      title = [];
+      event_type = [];
+      event_group = [];
+      more_info_url = [];
+      all_dates = [];
+      next_date = [];
+      lat = [];
+      long = [];
+
+      for (let i = 0; i < db.length; i++) {
+        address.push(db[i].address);
+        notes.push(db[i].notes);
+        title.push(db[i].title);
+        event_type.push(db[i].event_type);
+        more_info_url.push(db[i].more_info_url);
+        event_group.push(db[i].event_group);
+        all_dates.push(db[i].all_dates);
+        next_date.push(db[i].next_date_times);
+        lat.push(db[i].longitude);
+        long.push(db[i].longitude);
+      }
+
+      YourFunctionHere();
+    } else if (xhr.status == 404) {
+      console.log("Could not find database");
+    }
+  };
+  xhr.open("GET", "https://data.calgary.ca/resource/n625-9k5x.json", true);
+
+  xhr.send();
+}
+/**
+ * TODO
+ */
+function YourFunctionHere() {
+  //here you will add your populate into HTML from JS....
+  //basic for loop to print all to console
+  for (let i = 0; i < db.length; i++) console.log(title[i]);
+}
