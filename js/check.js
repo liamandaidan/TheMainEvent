fb72a5e462fb83d14151c0562587d6d83319e4d8
Martin <67307327+Interrius@users.noreply.github.com>
2022-07-24 09:01:24 -0600

    Check

--- js/check.js
+++ js/check.js
@@ -5,59 +5,16 @@ function checkfunction() {
     var check4 = document.getElementById("check4");
     var check5 = document.getElementById("check5");
     var check6 = document.getElementById("check6");
-    var message = document.getElementById("message");
-    var checked1 = document.getElementById("check1").value;  
-    var checked2 = document.getElementById("check2").value; 
-    var checked3 = document.getElementById("check3").value;  
-    var checked4 = document.getElementById("check4").value;  
-    var checked5 = document.getElementById("check5").value;
-    if (check1.checked == true&&check2.checked == false&&check3.checked == false&&check4.checked == false&&check5.checked == false&&check6.checked == false) {
-        alert("You will now be recieving message from the following categary: "+checked1);
-    } 
-    else if(check1.checked == true&&check2.checked == true&&check3.checked == false&&check4.checked == false&&check5.checked == false&&check6.checked == false){
-        var checked1 = document.getElementById("check1").value;  
-        var checked2 = document.getElementById("check2").value;  
-        alert("You will now be recieving message from the following categary: "+checked1+" , "+checked2 );
-    } else if(check1.checked == true&&check2.checked == true&&check3.checked == true&&check4.checked == false&&check5.checked == false&&check6.checked == false){
-        var checked1 = document.getElementById("check1").value;  
-        var checked2 = document.getElementById("check2").value; 
-        var checked3 = document.getElementById("check3").value;  
-        var checked4 = document.getElementById("check4").value;  
-        var checked5 = document.getElementById("check5").value;
-        alert("YYou will now be recieving message from the following categary: "+checked1+" , "+checked2+" , "+checked3);
-    } else if(check1.checked == true&&check2.checked == true&&check3.checked == true&&check4.checked == true&&check5.checked == false&&check6.checked == false){
-        var checked1 = document.getElementById("check1").value;  
-        var checked2 = document.getElementById("check2").value; 
-        var checked3 = document.getElementById("check3").value;  
-        var checked4 = document.getElementById("check4").value;  
-        var checked5 = document.getElementById("check5").value;
-        alert("You will now be recieving message from the following categary: "+checked1+" , "+checked2+" , "+checked3+" , "+checked4);
-    } else if(check1.checked == true&&check2.checked == true&&check3.checked == true&&check4.checked == true&&check5.checked == true&&check6.checked == false){
-        var checked1 = document.getElementById("check1").value;  
-        var checked2 = document.getElementById("check2").value; 
-        var checked3 = document.getElementById("check3").value;  
-        var checked4 = document.getElementById("check4").value;  
-        var checked5 = document.getElementById("check5").value; 
-        alert("You will now be recieving message from the following categary:"+checked1+" , "+checked2+" , "+checked3+" , "+checked4+" , "+checked5);
-    } else if(check1.checked == true&&check2.checked == true&&check3.checked == true&&check4.checked == true&&check5.checked == true&&check6.checked == true){
-        var checked1 = document.getElementById("check1").value;  
-        var checked2 = document.getElementById("check2").value; 
-        var checked3 = document.getElementById("check3").value;  
-        var checked4 = document.getElementById("check4").value;  
-        var checked5 = document.getElementById("check5").value;
-
-        alert("You will now be recieving message from the following categary:Other");
-    }else if(check1.checked == false&&check2.checked == true&&check3.checked == false&&check4.checked == false&&check5.checked == false&&check6.checked == false){
-        alert("You will now be recieving message from the following categary: "+checked2);
-    }else if(check1.checked == false&&check2.checked == true&&check3.checked == true&&check4.checked == false&&check5.checked == false&&check6.checked == false){
-        alert("You will now be recieving message from the following categary: "+checked2+","+checked3);
-        }else if(check1.checked == false&&check2.checked == true&&check3.checked == true&&check4.checked == true&&check5.checked == false&&check6.checked == false){
-            alert("You will now be recieving message from the following categary: "+checked2+","+checked3+","+checked4);
-            }else if(check1.checked == false&&check2.checked == true&&check3.checked == true&&check4.checked == true&&check5.checked == true&&check6.checked == false){
-                alert("You will now be recieving message from the following categary: "+checked2+","+checked3+","+checked4+","+checked5);
-                }else if(check1.checked == false&&check2.checked == true&&check3.checked == true&&check4.checked == true&&check5.checked == true&&check6.checked == true){
-                    alert("You will now be recieving message from the following categary: "+checked2+","+checked3+","+checked4+","+checked5);
-                    }else if(check1.checked == false&&check2.checked == false&&check3.checked == true&&check4.checked == false&&check5.checked == false&&check6.checked == false){
-                        alert("You will now be recieving message from the following categary: "+checked3);
-                    }
+    var text = "You will get the following notifications: ";
+    for(let i=1;i<6;i++){
+        
+        for (let i = 0; i < 6; i++) {
+            if((check+[i]).checked==true){
+            text += document.getElementById("check"+[i]).value; + "<br>";
+            console.log("Marti");
+          }
+        }
+      }
+      document.getElementById("message").innerHTML = text;
     }
+   
